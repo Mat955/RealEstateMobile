0d962eced09d49eba3c996d8c0521d69e50d9877
Mateusz Lewartowski <lewar2013c@gmail.com>
2025-01-15 14:16:58 +0100

    Add Filters component and enhance card layout in the main index screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import images from "../../../constants/images";
 import icons from "../../../constants/icons";
 import Search from "../../../components/Search";
 import {Card, FeaturedCard} from "../../../components/Cards";
+import Filters from "../../../components/Filters";
 
 export default function Index() {
   return (
@@ -34,9 +35,26 @@ export default function Index() {
               </Text>
             </TouchableOpacity>
           </View>
+          <View className='flex flex-row gap-5 mt-5'>
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
         </View>
-        <FeaturedCard />
-        <Card />
+        <View className='flex flex-row items-center justify-between'>
+          <Text className='text-xl font-rubik-bold text-black-300'>
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className='text-base font-rubik-bold text-primary-300'>
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Filters />
+        <View className='flex flex-row gap-5 mt-5'>
+          <Card />
+          <Card />
+        </View>
       </View>
     </SafeAreaView>
   );
