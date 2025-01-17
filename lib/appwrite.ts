ce7f6c3896798a4c8d96c3ddb74238e9626ee0ee
Mateusz Lewartowski <lewar2013c@gmail.com>
2025-01-17 14:43:02 +0100

    Implement property listing and detail features: integrate latest properties fetching, enhance card components with dynamic data, and add loading and no-results states.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -4,6 +4,7 @@ import {
   Client,
   Databases,
   OAuthProvider,
+  Query,
 } from "react-native-appwrite";
 import * as Linking from "expo-linking";
 import {openAuthSessionAsync} from "expo-web-browser";
@@ -13,10 +14,12 @@ export const config = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
-  galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_ID,
-  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_ID,
-  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_ID,
-  propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -28,7 +31,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
-export const database = new Databases(client);
+export const databases = new Databases(client);
 
 export async function login() {
   try {
@@ -70,7 +73,7 @@ export async function login() {
 
 export async function logout() {
   try {
-    await account.deleteSessions("current");
+    await account.deleteSessions();
     return true;
   } catch (error) {
     console.error(error);
@@ -95,5 +98,63 @@ export async function getCurrentUser() {
   } catch (error) {
     console.error(error);
     return null;
+  }
+}
+
+export async function getLatestProperties() {
+  try {
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      [Query.orderAsc("$createdAt"), Query.limit(5)]
+    );
+
+    return result.documents;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export async function getProperties({
+  filter,
+  query,
+  limit,
+}: {
+  filter?: string;
+  query?: string;
+  limit?: number;
+}) {
+  try {
+    const buiildQuery = [Query.orderDesc("$createdAt")];
+
+    if (filter && filter !== "all") {
+      buiildQuery.push(Query.equal("type", filter));
+    }
+
+    if (query) {
+      buiildQuery.push(
+        Query.or([
+          Query.search("name", query),
+          Query.search("type", query),
+          Query.search("address", query),
+        ])
+      );
+    }
+
+    if (limit) {
+      buiildQuery.push(Query.limit(limit));
+    }
+
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      buiildQuery
+    );
+
+    return result.documents;
+  } catch (error) {
+    console.log(error);
+    return [];
   }
 }
